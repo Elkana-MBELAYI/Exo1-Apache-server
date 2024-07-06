880fe006870ff8b9b1dc9d567c091f536e87e3f5
Elkana MBELAYI <113510425+Elkana-MBELAYI@users.noreply.github.com>
2024-07-06 10:18:54 +0100

    Create help.php

---
+++ help.php
@@ -0,0 +1,34 @@
+<?php
+    // Ecrit le log dans mon fichier
+    error_log("Elkach, go to the help page...");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon site web</title>
+    <style>
+        body {
+            background-color: cyan;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            color: white;
+            font-family: Arial, sans-serif;
+        }
+
+        .card {
+            background-color: #222;
+            padding: 25px;
+            border-radius: 9px;
+        }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1>Do you want any help?</h1>
+    </div>
+</body>
