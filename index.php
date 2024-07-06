ec8b818889e5e94e70342fe9a6d682c27e9c503f
Elkana MBELAYI <113510425+Elkana-MBELAYI@users.noreply.github.com>
2024-07-06 10:22:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <style>
+        body {
+            background-color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            color: white;
+            font-family: Arial, sans-serif;
+        }
+
+        .card {
+            background-color: #222;
+            padding: 20px;
+            border-radius: 6px;
+        }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1>Welcome to the home PHP</h1>
+    </div>
+</body>
+</html>
